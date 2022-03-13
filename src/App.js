37c3964bb2787cc3fd8eaf1746010df8b3621fcf
huulamnguyen <liamnguyen.swe@gmail.com>
2022-03-12 20:04:18 -0500

    04-hover-focus-other-states

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             Lorem Ipsum work from Lorem Ipsum is Lorem Ipsum and Lorem Ipsum is a utility for helping others to discover the meaning of their lives.
           </p>
           <div className="mt-4 sm:mt-6">
-            <a className="inline-block px-5 py-3 rounded-lg bg-indigo-500 text-white shadow-lg uppercase tracking-wider font-semibold text-sm sm:text-base" href="#">Book your escape</a>
+            <a className="inline-block px-5 py-3 rounded-lg bg-indigo-500 hover:bg-indigo-400 sm:hover:bg-green-400 hover:-translate-y-0.5 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-indigo-500 focus:ring-opacity-50 active:bg-indigo-600 transform transition text-white shadow-lg uppercase tracking-wider font-semibold text-sm sm:text-base" href="#">Book your escape</a>
           </div>
         </div>
       </div>
